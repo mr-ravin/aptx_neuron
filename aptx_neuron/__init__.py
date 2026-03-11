5026c6f0c5ee040417ab3c7ce8faa32f4f79c87d
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2026-03-11 11:04:26 +0530

    Update __init__.py

--- aptx_neuron/__init__.py
+++ aptx_neuron/__init__.py
@@ -1,25 +1,31 @@
 import torch
 import torch.nn as nn
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 # -----------------------------------
 # APTx Neuron
 # -----------------------------------
 class aptx_neuron(nn.Module):
-    def __init__(self, input_dim, is_alpha_trainable=True):
+    def __init__(self, input_dim, is_alpha_trainable=True, use_delta=True):
         super().__init__()
+        self.use_delta = use_delta
         if is_alpha_trainable:
             self.alpha = nn.Parameter(torch.randn(input_dim))
         else:
             self.register_buffer('alpha', torch.ones(input_dim))
         self.beta = nn.Parameter(torch.randn(input_dim))
         self.gamma = nn.Parameter(torch.randn(input_dim))
-        self.delta = nn.Parameter(torch.zeros(1))
+        if self.use_delta:
+            self.delta = nn.Parameter(torch.zeros(1))
+        else:
+            self.register_parameter("delta", None)
 
     def forward(self, x):  # x: [batch_size, input_dim]
         nonlinear = (self.alpha + torch.tanh(self.beta * x)) * self.gamma * x
-        y = nonlinear.sum(dim=1, keepdim=True) + self.delta
+        y = nonlinear.sum(dim=1, keepdim=True)
+        if self.use_delta:
+            y = y + self.delta
         return y
 
 
@@ -27,19 +33,21 @@ class aptx_neuron(nn.Module):
 # APTx Layer (Vectorized Multiple Neurons)
 # -----------------------------------
 class aptx_layer(nn.Module):
-    def __init__(self, input_dim, output_dim, is_alpha_trainable=True):
+    def __init__(self, input_dim, output_dim, is_alpha_trainable=True, use_delta=True):
         super().__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
-
+        self.use_delta = use_delta
         if is_alpha_trainable:
             self.alpha = nn.Parameter(torch.randn(output_dim, input_dim))
         else:
             self.register_buffer('alpha', torch.ones(output_dim, input_dim))
-
         self.beta = nn.Parameter(torch.randn(output_dim, input_dim))
         self.gamma = nn.Parameter(torch.randn(output_dim, input_dim))
-        self.delta = nn.Parameter(torch.zeros(output_dim))
+        if self.use_delta:
+            self.delta = nn.Parameter(torch.zeros(output_dim))
+        else:
+            self.register_parameter("delta", None)
 
     def forward(self, x):  # x: [batch_size, input_dim]
         # x -> [batch_size, 1, input_dim]
@@ -50,5 +58,7 @@ class aptx_layer(nn.Module):
         ) * self.gamma.unsqueeze(0) * x_exp
 
         # [batch_size, output_dim]
-        y = nonlinear.sum(dim=2) + self.delta
+        y = nonlinear.sum(dim=2)
+        if self.use_delta:
+            y = y + self.delta
         return y
